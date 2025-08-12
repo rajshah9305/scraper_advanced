2693d6e4366e8b6878052224c94c258fdcc9b28c
Raj Shah <rajshah9305@gmail.com>
2025-08-12 02:34:29 -0600

    Update flask_scraper_app.py

--- flask_scraper_app.py
+++ flask_scraper_app.py
@@ -471,4 +471,5 @@ def health_check():
 
 if __name__ == '__main__':
     logger.info("Starting Elite Web Scraper Flask application...")
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+   port = int(os.environ.get('PORT', 5000))
+socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
